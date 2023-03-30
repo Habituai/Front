5f07e2ee82cba8caa91c47a854e1f84cef4fcf16
Bruno Franco <brunofrancodemoraes@gmail.com>
2023-03-30 11:46:20 -0300

    create auth context and hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
 import "./styles/global.css";
-import { Outlet } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import Home from "./pages/Home";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
+import Dashboard from "./pages/Dashboard";
+import { useAuth } from "./hooks/useAuth";
 
 function App() {
-	return <Outlet />;
+	const { auth } = useAuth();
+
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route exact path="/" element={<Home />} />
+				<Route path="/sign-up" element={<SignUp />} />
+				<Route path="/sign-in" element={<SignIn />} />
+				{auth && <Route path="/dashboard" element={<Dashboard />} />}
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,42 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import AuthProvider from "./contexts/AuthContext";
 
 import App from "./App";
-import Home from "./pages/Home";
-import ErrorPage from "./pages/404";
-import Dashboard from "./pages/Dashboard";
-import SignIn from "./pages/SIgnIn";
-import SignUp from "./pages/SignUp";
-
-const router = createBrowserRouter([
-	{
-		path: "/",
-		element: <App />,
-		errorElement: <ErrorPage />,
-		children: [
-			{
-				path: "/",
-				element: <Home />,
-			},
-			{
-				path: "/dashboard",
-				element: <Dashboard />,
-			},
-			{
-				path: "/sign-in",
-				element: <SignIn />,
-			},
-			{
-				path: "/sign-up",
-				element: <SignUp />,
-			},
-		],
-	},
-]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
-		<RouterProvider router={router} />
+		<AuthProvider>
+			<App />
+		</AuthProvider>
 	</React.StrictMode>
 );

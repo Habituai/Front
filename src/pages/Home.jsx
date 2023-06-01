8e9015e3a411b7fc43f70167b6425426f8566316
Bruno Franco <brunofrancodemoraes@gmail.com>
2023-05-31 23:06:03 -0300

    create base home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,28 +1,56 @@
+import { Button } from "@mui/material";
 import { Link } from "react-router-dom";
+import toDoImage from "../assets/images/todo.svg";
 
 function Home() {
     return (
-        <header className="w-full py-5 flex justify-center">
-            <nav className="w-3/4 flex justify-between items-center">
-                <span className="text-6xl text-primaryDark font-bold">
+        <section className="w-full h-screen flex flex-col px-12 bg-primaryDark">
+            <header className="w-full flex justify-between py-4">
+                <h1 className="text-6xl text-secondaryLight font-bold">
                     Habituaí
-                </span>
-                <div className="flex justify-between items-center gap-8">
-                    <Link
-                        className="text-2xl text-primaryMedium hover:text-primaryExtraLight font-semibold"
-                        to="/sign-up"
-                    >
-                        Cadastrar
+                </h1>
+
+                {/* <nav className="flex justify-between items-center gap-8">
+                    <span className="text-md text-gray-200 hover:text-gray-400 cursor-pointer">
+                        Sobre
+                    </span>
+
+                    <span className="text-md text-gray-200 hover:text-gray-400 cursor-pointer">
+                        Desenvolvedores
+                    </span>
+                </nav> */}
+
+                <nav className="flex justify-between items-center gap-8">
+                    <Link to="/sign-up">
+                        <Button variant="contained" color="good" size="large">
+                            Crie sua conta
+                        </Button>
                     </Link>
-                    <Link
-                        className="text-2xl text-primaryMedium hover:text-primaryExtraLight font-semibold"
-                        to="/sign-in"
-                    >
-                        Login
+                    <Link to="/sign-in">
+                        <Button variant="outlined" color="good" size="large">
+                            Já possuo conta
+                        </Button>
                     </Link>
+                </nav>
+            </header>
+
+            <div className="w-full h-full flex justify-center items-center">
+                <div className="w-full flex flex-1 flex-col gap-12">
+                    <h3 className="text-secondaryExtraLight font-bold text-7xl">
+                        QUER CRIAR NOVOS HÁBITOS?
+                    </h3>
+
+                    <p className="text-gray-300 text-xl">
+                        Com o Habituaí você pode gerenciar e organizar uma
+                        semana completa de hábitos
+                    </p>
                 </div>
-            </nav>
-        </header>
+
+                <div className="w-full flex flex-1 flex-col">
+                    <img src={toDoImage} />
+                </div>
+            </div>
+        </section>
     );
 }
 

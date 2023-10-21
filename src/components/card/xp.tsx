365701c85d92585b401895c800aea1ad0086ac53
Bruno Franco <brunofrancodemoraes@gmail.com>
2023-10-21 13:08:55 -0300

    adjust xp font size

--- src/components/card/xp.tsx
+++ src/components/card/xp.tsx
@@ -4,7 +4,7 @@ interface ExperienceCardProps {
 
 export default function ExperienceCard({ experience }: ExperienceCardProps) {
     return (
-        <span className="py-2 px-4 text-sm xl:text-xl bg-primaryExtraLight text-white rounded-lg shadow-md flex justify-center items-center gap-1">
+        <span className="py-2 px-4 text-2xl bg-primaryExtraLight text-white rounded-lg shadow-md flex justify-center items-center gap-1">
             {experience}
             <span className="text-secondaryExtraLight font-bold">xp</span>
         </span>

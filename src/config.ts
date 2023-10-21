a2c03d0f735fc918875a6eb21a1f32d72f215e49
Bruno Franco <brunofrancodemoraes@gmail.com>
2023-10-21 11:15:10 -0300

    add config default values

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,9 @@
 export const envs = {
-    baseURL: import.meta.env.VITE_HOST_BACKEND,
-    loginPath: import.meta.env.VITE_LOGIN_PATH,
-    userPath: import.meta.env.VITE_USER_PATH,
-    habitPath: import.meta.env.VITE_HABIT_PATH,
-    habitWeekPath: import.meta.env.VITE_HABIT_WEEK_PATH,
-    progressPath: import.meta.env.VITE_PROGRESS_PATH,
-    userAccountPath: import.meta.env.VITE_USER_ACCOUNT_PATH,
+    baseURL: import.meta.env.VITE_HOST_BACKEND || 'http://localhost:8080',
+    loginPath: import.meta.env.VITE_LOGIN_PATH || '/api/v1/user/login',
+    userPath: import.meta.env.VITE_USER_PATH || '/api/v1/user',
+    habitPath: import.meta.env.VITE_HABIT_PATH || '/api/v1/habit',
+    habitWeekPath: import.meta.env.VITE_HABIT_WEEK_PATH || '/api/v1/habit/week',
+    progressPath: import.meta.env.VITE_PROGRESS_PATH || '/api/v1/progress',
+    userAccountPath: import.meta.env.VITE_USER_ACCOUNT_PATH || '/api/v1/user/account',
 };
